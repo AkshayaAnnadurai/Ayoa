50ebec7522475dc9d6f31822d9e14411458f1565
Your NameAkshaya Annadurai <akshaysrij@gmail.com>
2022-07-23 03:20:51 +0530

    home

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -6,8 +6,10 @@ function Home() {
         <div >
             <div id='center' >
                 <h1 id='head'>Be different, work visually</h1>
-                <p className='p'>Try the world’s most visual productivity app. Supercharge thinking, planning and </p> <br />
-                <p className='p'>learning with AI, mind maps, task boards and more</p>
+                <div id="pa">
+                <p className='p'>Try the world’s most visual productivity app. Supercharge thinking, planning and 
+               learning with AI, mind maps, task boards and more</p>
+               </div>
                 <div id='in'>
 
                     <input className='input' type="text" placeholder='Enter your email' />
@@ -18,20 +20,164 @@ function Home() {
             <div >
                 <img src="https://www.ayoa.com/wp-content/uploads/2021/11/ayoa_allinone.webp" alt="AD" />
             </div>
-            <div>
+            <div id="cont">
+                <div>
                 <img src='https://www.ayoa.com/wp-content/uploads/2018/05/cbsnews_logo.png' alt='logo' />
                 <p>“The most unique twist on task management I’ve ever seen”</p>
+                </div>
+                <div>
                 <img src='https://www.ayoa.com/wp-content/uploads/2018/05/techcrunch-logo.png' alt='logo' />
                 <p>“Visually-oriented people – this layout boosts productivity”</p>
+                
+                </div>
+                <div>
                 <img src='https://www.ayoa.com/wp-content/uploads/2019/09/logo-lhacker.png' alt='logo' />
                 <p>“Turns your tasks into an awesome venn diagram”</p>
+                </div>
+                <div>
                 <img src='https://www.ayoa.com/wp-content/uploads/2019/09/logo-pcworld.png' alt='logo' />
                 <p>“A more intuitive way to manage workloads”</p>
+                </div>
             </div>
-            <div>
+            <div id="cont2">
+                <div id="vid">
+                    <h1>Vid</h1>
+                    {/* <video> 
+                    <source src="https://www.ayoa.com/wp-content/uploads/2019/07/ayoa_webcut_video.mp4">
+                        </source>
+                    </video> */}
+                </div>
+            <div id="secdiv">
                 <h1>See Ayoa in action</h1>
                 <p>The all-in-one mind mapping, collaboration and task management app. See how you can achieve your best work.</p>
-                <button>Watch Video</button>
+                <button id="btnn">Watch Video</button>
+            </div>
+            </div>
+            <div id="cont3">
+<h2 id="sp">Unrivaled <span >flexibility</span></h2>
+<div id="pa">
+<p>Whether you’re brainstorming ideas, presenting in meetings, or managing tasks, Ayoa provides you with flexibility and intuitive working methods every step of the way.</p>
+</div>
+            </div>
+            <div id="cont4">
+                <div>
+                    <img className="imgg" src="https://www.ayoa.com/wp-content/uploads/2020/10/organic.svg" alt="piv" />
+                    <p className='pas'>Mind maps</p>
+                </div>
+                <div>
+                <img className="imgg" src="https://www.ayoa.com/wp-content/uploads/2020/10/whiteboard.svg" alt="piv" />
+                    <p className="pas">Whiteboards</p>
+                </div>
+                <div>
+                <img className="imgg" src="https://www.ayoa.com/wp-content/uploads/2020/10/workflow.svg" alt="piv" />
+                    <p className="pas">Workflow</p>
+                </div>
+                <div>
+                <img className="imgg" src="https://www.ayoa.com/wp-content/uploads/2020/10/canvas.svg"  alt="piv"/>
+                    <p className="pas">Canvas</p>
+                </div>
+                <div>
+                <img className="imgg" src="https://www.ayoa.com/wp-content/uploads/2020/10/gantt.svg" alt="piv"/>
+                    <p className="pas">Gantt</p>
+                </div>
+                <div>
+                <img className="imgg" src="https://www.ayoa.com/wp-content/uploads/2020/10/radial.svg" alt="piv"/>
+                    <p className="pas">Radial</p>
+                </div>
+                <div>
+                <img className="imgg" src="https://www.ayoa.com/wp-content/uploads/2022/06/catpuremaps.svg" alt="piv" />
+                    <p className="pas">Capture</p>
+                </div>
+            </div>
+            <div id="cont5">
+                <div id="c51">
+                     <h1>Whiteboards</h1>
+<div id="paa">
+                     <p>  Use whiteboards for freeform thinking or to create content dashboards. Add post-it notes, images, flowcharts, links, videos and more.</p>
+                     
+                     <div id="kty">
+                        <div>
+                    <img  id="vd" src="https://www.ayoa.com/wp-content/uploads/2021/11/wb_thumbnail_new.png" alt="vid" />
+                    </div>
+                    <div>
+                    <p>How to use whiteboards</p>
+                    <p>01:56</p>
+                    </div>
+                     </div>
+                     </div>
+                     </div>
+                <div id="c52">
+                <img id="phot" src="https://www.ayoa.com/wp-content/uploads/2021/03/Unrivaled_wb_new_webp_2.webp" alt="pc" />
+                </div>
+            </div>
+            <div id="cont6">
+                <div id="c61">
+             <h1>The world’s only
+mind maps with AI</h1>
+</div>
+<div id="c62">
+                <p> 
+Use one of the largest deep learning networks ever created to generate unique and innovative ideas for anything you can think of.</p>
+               
+</div>
+<div id="flex">
+<div id="c63">
+                <ul>
+                    <li className='li'>
+                        <img src="https://www.ayoa.com/wp-content/uploads/2021/10/tick.svg" alt="tick"/>
+                        <p>Ask the right questions</p>
+                    </li>
+                    <li className='li'>
+                        <img src="https://www.ayoa.com/wp-content/uploads/2021/10/tick.svg" alt="tick"/>
+                        <p>Breeze past blocks</p>
+                    </li>
+                    <li className='li'>
+                        <img src="https://www.ayoa.com/wp-content/uploads/2021/10/tick.svg" alt="tick"/>
+                        <p>Explore new angles</p>
+                    </li>
+                    </ul>
+                    </div>
+                    <div id="c64">
+                        <ul>
+                    <li className='li'>
+                        <img src="https://www.ayoa.com/wp-content/uploads/2021/10/tick.svg" alt="tick"/>
+                        <p>Unlock problems</p>
+                    </li>
+                    <li className='li'>
+                        <img src="https://www.ayoa.com/wp-content/uploads/2021/10/tick.svg" alt="tick"/>
+                        <p>Mind map in full flow</p>
+                    </li>
+                    </ul>
+                    </div>
+               
+               </div>
+            </div>
+            <div id="cont7">
+                <div>
+                    <img  id="im" src="https://www.ayoa.com/wp-content/uploads/2021/11/diverse_teams_ayoa-1024x862.webp" alt="pi" />
+                </div>
+                <div id="c72">
+                    <h1>Designed for diversity of thought</h1>
+                    <p>Diverse ideas discover alternative paths, they break moulds, they lead to breakthroughs. Discover how Ayoa’s neuro-inclusive design makes it a tool which helps different types of people work together to achieve their best work.</p>
+                    <div id="c73">
+                        <img id="lg1" src="https://www.ayoa.com/wp-content/uploads/2021/07/ayoa_assistive_tech-300x120.png " alt="logo" />
+                        <img  id="lg2" src=" https://www.ayoa.com/wp-content/uploads/2021/07/bata_logo-234x300.png" alt="logo" />
+                        <img  id="lg3" src="https://www.ayoa.com/wp-content/uploads/2021/07/bda_assured@2x-300x157.png " alt="logo" />
+                    </div>
+                </div>
+            </div>
+            <div id="cont8">
+                <div id="c81">
+                <h1>Try</h1>
+                <img id="lv"src="https://www.ayoa.com/wp-content/uploads/2019/11/ayoa_logo.png" alt="logo" />
+                <h1>today</h1>
+                </div>
+                <div id="c88">
+                <div id="c82">
+                    <input type="email" placeholder='Enter your email' />
+                    <button>Get Started</button>
+                </div>
+                </div>
             </div>
         </div>
     )
